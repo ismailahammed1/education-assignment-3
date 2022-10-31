7e3843e3439b41b154bbd20e40d73b9ea4c47e8c
Md Ismail Ahammed Roman <ismailahmmedroman@gmail.com>
2022-10-31 16:38:26 +0600

    data last update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 4000;
 
 app.use(cors());
 const language = require("./data/languages.json");
-const course = require("./data/languages.json");
+
 app.get("/", (req, res) => {
   res.send("look mama!");
 });
@@ -15,21 +15,12 @@ app.get("/", (req, res) => {
 app.get("/language", (req, res) => {
   res.send(language);
 });
-app.get("/course", (req, res) => {
-  res.send(course);
-});
 
 app.get("/language/:id", (req, res) => {
   const id = parseInt(req.params.id);
   console.log("looking for id for", id);
   const languages = language.find((ph) => ph.id === id);
   res.send(languages);
-});
-app.get("/course/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  console.log("looking for id for", id);
-  const courses = course.find((ph) => ph.id === id);
-  res.send(courses);
 });
 
 app.listen(port, () => {
