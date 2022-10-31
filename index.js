b0889d36e384626911766f7aa2a28789f9b00a2c
Md Ismail Ahammed Roman <ismailahmmedroman@gmail.com>
2022-10-31 16:39:57 +0600

    data last up

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 4000;
 
 app.use(cors());
 const language = require("./data/languages.json");
-
+//update
 app.get("/", (req, res) => {
   res.send("look mama!");
 });
